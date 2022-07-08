51add5551a6a0eb39ebb21a4b7269f7f0d083511
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-08 22:01:58 +0100

    complete get one post request (w/o login)

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -22,3 +22,22 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// get one post by its id
+router.get("/post/:id", async (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.id, {
+      include: [User, { model: Comment, include: [User] }],
+    });
+
+    if (!dbPostData) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+
+    res.status(200).json(dbPostData);
+    // TODO: withAuth, user need to login before showing the post
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
