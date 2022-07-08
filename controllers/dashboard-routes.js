217f058a2219b7ea46d527fc694805c47ad7f9d2
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-08 21:40:01 +0100

    export routers

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,3 +2,5 @@ const router = require("express").Router();
 const { Comment, Post, User } = require("../models");
 // Import the custom middleware
 const withAuth = require("../utils/auth");
+
+module.exports = router;
