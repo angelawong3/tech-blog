f8b2bb0734ce321c4b6a40bfbbcf3df86e421c1a
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-08 21:03:29 +0100

    association done

--- models/index.js
+++ models/index.js
@@ -2,4 +2,32 @@ const Comment = require("./Comment");
 const Post = require("./Post");
 const User = require("./User");
 
+User.hasMany(Post, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "postId",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "userId",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "postId",
+});
+
 module.exports = { Comment, Post, User };
