2adad2c5320c36745e2027ec0619bfe09f506645
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-08 21:53:32 +0100

    complete get all posts request in home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,3 +4,21 @@ const { Comment, Post, User } = require("../models");
 const withAuth = require("../utils/auth");
 
 module.exports = router;
+
+// get all posts
+router.get("/", async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      include: [User],
+    });
+    // const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+    // res.render("homepage", {
+    //   posts,
+    //   loggedIn: req.session.loggedIn,
+    // });
+    res.status(200).json(dbPostData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
