d8e43f8c04b695dd01cb7026ea9ada97325d2ef9
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-10 15:25:25 +0100

    to add new post done

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,7 +3,7 @@ const { Comment, Post, User } = require("../models");
 // Import the custom middleware
 const withAuth = require("../utils/auth");
 
-// get all posts
+// get all user's posts when logged in
 router.get("/", withAuth, async (req, res) => {
   try {
     const dbPostData = await Post.findAll({
@@ -17,6 +17,34 @@ router.get("/", withAuth, async (req, res) => {
       posts,
       loggedIn: req.session.loggedIn,
     });
+  } catch (err) {
+    res.redirect("login");
+  }
+});
+
+// to create new post
+router.get("/new", withAuth, (req, res) => {
+  res.render("new-post", {
+    layout: "dashboard",
+    loggedIn: req.session.loggedIn,
+  });
+});
+
+// to edit existing post
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.id);
+
+    if (dbPostData) {
+      const post = dbPostData.get({ plain: true });
+      res.render("edit-post", {
+        layout: "dashboard",
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    } else {
+      res.status(404).end();
+    }
   } catch (err) {
     res.redirect("login");
   }
