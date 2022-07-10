cf3d446004dd9760943e02ecc4a8cfbc853bd095
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-10 22:35:36 +0100

    enable users to login and signup

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,13 +3,65 @@ const { User } = require("../../models");
 
 // "/api/user" endpoint
 
-// POST request "/"
-// to sign up
+// POST request to sign up
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
 
-// POST request "/login"
-// to login
+    req.session.save(() => {
+      req.session.userId = userData.id;
+      req.session.logged_in = true;
 
-// POST request"/logout"
-// to logout
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// POST request to login
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password, please try again" });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// POST request to logout
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,13 +1,13 @@
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector("#name-signup").value.trim();
+  const username = document.querySelector("#name-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
   if (name && password) {
     const response = await fetch("/api/user", {
       method: "POST",
-      body: JSON.stringify({ name, password }),
+      body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
     });
 
