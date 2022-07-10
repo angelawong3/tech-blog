edcc789169c961470e007cd20ed36083c0771fda
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-10 22:45:50 +0100

    enable user to create, update, and del post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,15 +1,61 @@
 const router = require("express").Router();
 const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // "/api/post" endpoint
 
-// POST request "/"
-// to add a new post
+// POST request to add a new post
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      userId: req.session.userId,
+    });
 
-// PUT request "/:id"
-// to edit an existing post
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-// DELETE request ":id"
-// to del an exisiting post
+// PUT request to edit an existing post
+router.put("/:id", async (req, res) => {
+  try {
+    const [editedPost] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (editedPost > 0) {
+      res.status(200).json(editedPost);
+    } else {
+      res.status(404).json({ message: "Cannot edit post, please try again!" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE request to del an exisiting post
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        userId: req.session.userId,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
