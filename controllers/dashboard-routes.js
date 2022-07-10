fc91fbbc4755549fa9970004b360fc9421e2de2f
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-10 15:07:06 +0100

    get all post by userId in dashboard page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,4 +3,23 @@ const { Comment, Post, User } = require("../models");
 // Import the custom middleware
 const withAuth = require("../utils/auth");
 
+// get all posts
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      where: { userId: req.session.userId },
+      include: [User],
+    });
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+    res.render("all-post", {
+      layout: "dashboard",
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.redirect("login");
+  }
+});
+
 module.exports = router;
