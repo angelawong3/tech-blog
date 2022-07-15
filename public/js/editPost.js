1abbb1c9fb9f2b5195e5134be21f26bc7cecc8a9
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-15 13:35:55 +0100

    tested del post on server

---
+++ public/js/editPost.js
@@ -0,0 +1,51 @@
+const editFormHandler = async (event) => {
+  event.preventDefault();
+
+  const postId = document.querySelector("#postId").value;
+  const userId = document.querySelector("#userId").value;
+  const postTitle = document.querySelector("#post-title").value;
+  const postContent = document.querySelector("#post-content").value;
+
+  const response = await fetch(`/api/post/${postId}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      postId,
+      userId,
+      postTitle,
+      postContent,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert("Failed to edit your post");
+  }
+  document.location.replace("/dashboard");
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+
+    const response = await fetch(`/api/post/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to delete post");
+    }
+  }
+};
+
+document
+  .querySelector("#edit-post-form")
+  .addEventListener("submit", editFormHandler);
+document
+  .querySelector("#delete-btn")
+  .addEventListener("click", delButtonHandler);
