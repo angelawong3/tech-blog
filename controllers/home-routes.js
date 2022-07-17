1659c0b2a4652d3852520d6f19372f61eba66598
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-17 22:41:04 +0100

    can logout after login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,7 +11,7 @@ router.get("/", async (req, res) => {
     });
     const posts = dbPostData.map((post) => post.get({ plain: true }));
 
-    res.render("all-post", { posts });
+    res.render("all-post", { posts, logged_in: req.session.logged_in });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -40,7 +40,6 @@ router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect("/dashboard");
-    // res.render("dashboard-posts");
     return;
   }
 
